c5bc6c0b6e38a6cb38b7d3da35c3c7365fa6fbbe
Tan Jia Xian <maxxontan@gmail.com>
2022-01-16 01:08:07 -0700

    Fix bugs and font issues

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,18 @@
 const DEFAULT_ZOOM_FACTORS = [0.1, 0.4, 0.8, 1.5, 2, 3, 3.5, 4];
 const generate = document.getElementById("generate");
 const screenshots = document.getElementsByClassName("screenshot");
+let isClicked = false;
 
 generate.addEventListener("click", () => {
+  if (isClicked) return;
+  else isClicked = true;
   let index = 0;
 
   //Zoom factor: 1 = 100%, 0.5 = 50%, 2 = 200%
   const interv = setInterval(() => {
     if (DEFAULT_ZOOM_FACTORS[index] >= 3.5) {
       chrome.tabs.setZoom(1);
+      isClicked = false;
       clearInterval(interv);
     } else {
       let img = document.createElement("img");

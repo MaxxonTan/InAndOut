d5bce33a8b4b85b3b07e3ab7a243b265df0c5af4
Tan Jia Xian <maxxontan@gmail.com>
2022-01-15 22:33:10 -0700

    Changed element id name

--- src/popup.js
+++ src/popup.js
@@ -1,30 +1,42 @@
-const headerImg = document.querySelector(".ino-logo");
-let changeColor = document.querySelector(".button");
-options = [""];
+const generate = document.getElementById("generate");
+const screenshots = document.getElementsByClassName("screenshot");
 
-console.log(headerImg);
+generate.addEventListener("click", () => {
+  console.log("CLICK");
 
   //Zoom factor: 1 = 100%, 0.5 = 50%, 2 = 200%
-  zoomFactor = 0.15;
-  const interv = setInterval(() => {
-    if (zoomFactor >= 3) {
-      console.log("DONE");
-      chrome.tabs.setZoom(1);
-      clearInterval(interv);
-    } else {
-      let img = document.createElement("img");
-      img.height = 150;
-      img.width = 200;
+  // zoomFactor = 0.15;
+  // const interv = setInterval(() => {
+  //   if (zoomFactor >= 3) {
+  //     console.log("DONE");
+  //     chrome.tabs.setZoom(1);
+  //     clearInterval(interv);
+  //   } else {
+  //     let img = document.createElement("img");
 
-      chrome.tabs.setZoom(zoomFactor);
-      setTimeout(() => {
-        chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
-          img.src = dataUrl;
-          document.querySelector("body").appendChild(img);
-          zoomFactor += 0.5;
-        });
-      }, 50);
-      console.log("HEre");
-    }
-  }, 550);
+  //     chrome.tabs.setZoom(zoomFactor);
+  //     setTimeout(() => {
+  //       chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+  //         img.src = dataUrl;
+  //         document.querySelector("body").appendChild(img);
+  //         zoomFactor += 0.5;
+  //       });
+  //     }, 50);
+  //     console.log("HEre");
+  //   }
+  // }, 550);
+
+  console.log(screenshots);
+  // for (let zf = 0.15; zf <= 3.5; zf += 0.5) {
+  //   if (zf == 0.15) {
+  //     chrome.tabs.setZoom(zoomFactor);
+  //     setTimeout(() => {
+  //       chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+  //         img.src = dataUrl;
+  //         document.querySelector("body").appendChild(img);
+  //         zoomFactor += 0.5;
+  //       });
+  //     }, 10);
+  //   }
+  // }
 });

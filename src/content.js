1f4e8adc1eb14633eaa9a26064a2b55a386978d8
Lefan <lefantan@hotmail.com>
2022-01-15 22:11:11 -0700

    small updates

--- src/content.js
+++ src/content.js
@@ -2,7 +2,12 @@ fetch(chrome.runtime.getURL("/popup.html"))
   .then((res) => res.text())
   .then(async (html) => {
     // Check if already exist, if so, remove hide-container class
-    document.body.insertAdjacentHTML("afterend", html);
+    const container = document.getElementsByClassName("ino-container");
+    if (container && container.length > 0) {
+      container[0].classList.remove("hide-container");
+    } else {
+      document.body.insertAdjacentHTML("afterend", html);
+    }
 
     const fontStyle = document.createElement("style");
     fontStyle.textContent = `@font-face { 
@@ -20,7 +25,6 @@ fetch(chrome.runtime.getURL("/popup.html"))
     const minimizeImg = document.querySelector("#hide > img");
     minimizeImg.src = chrome.runtime.getURL("/images/x.png");
 
-    const container = document.getElementsByClassName("ino-container");
     const hideButton = document.getElementById("hide");
     hideButton.addEventListener("click", () => {
       container[0].classList.add("hide-container");
